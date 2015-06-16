587ad51cdd7180a0e25c89ddc0d8384c1bcaf66d
Sean King <sean@seanking.org>
2015-06-16 15:47:32 +1000

    fix commenting after refactor

--- index.js
+++ index.js
@@ -5,45 +5,46 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-position', function () {
   return function (css) {
 
+    // build our plugin handler
     var ruleHandler = function(decl) {
 
-        // get the declaration and its values
-        var type = decl.prop,
-            vals = [],
-            pos = ['top', 'right', 'bottom', 'left'],
-            types = ['relative', 'absolute', 'fixed'];
+      // define declarations and values
+      var type = decl.prop,
+          vals = [],
+          pos = ['top', 'right', 'bottom', 'left'],
+          types = ['relative', 'absolute', 'fixed'];
 
-        if(types.indexOf(type) === -1) {
-          return false;
-        }
+      // only process our custom declarations
+      if(types.indexOf(type) === -1) {
+        return false;
+      }
 
-        vals = decl.value.split(' ');
+      // put the values into an array
+      vals = decl.value.split(' ');
 
-        // if only two values decalred, assign them to top|bottom right|left
-        if ( vals.length === 2 ) {
-            vals[2] = vals[0];
-            vals[3] = vals[1];
-        }
+      // if only two values decalred, assign them to top|bottom right|left
+      if ( vals.length === 2 ) {
+          vals[2] = vals[0];
+          vals[3] = vals[1];
+      }
 
-        // create the position-type declaration
-        decl.cloneBefore({ prop: 'position', value: type });
+      // create the position-type declaration
+      decl.cloneBefore({ prop: 'position', value: type });
 
-        // and each position offset
-        for ( var i = 0, k = pos.length; i < k; i++) {
-            decl.cloneBefore({ prop: pos[i], value: vals[i] });
-        }
+      // and each position offset
+      for ( var i = 0, k = pos.length; i < k; i++) {
+          decl.cloneBefore({ prop: pos[i], value: vals[i] });
+      }
 
-        // remove our custom declaration
-        decl.removeSelf();
+      // remove our custom declaration
+      decl.removeSelf();
 
     };
 
-    // loop through each css rule
+    // loop through each css rule and declaration, and run our plugin through them
     css.eachRule(function(rule) {
-
-      // and each declaration in that rule
       rule.each(ruleHandler);
-
     });
+
   };
 });
